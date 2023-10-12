22c163002f861ab5d8c9a8f97690d7fb828e685a
snsd904513 <snsd904513@gmail.com>
2023-10-12 11:07:37 +0900

    TopServlet.java、top.jsp変更

---
+++ Aproject3/WebContent/top.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>健康管理アプリ</title>
+</head>
+<body>
+<h1>健康管理アプリ</h1>
+<ul>
+	<li><a href="PastdataServlet">過去のデータ</a></li>
+	<li><a href="GoalServlet">目標設定</a></li>
+	<li><a href="AccountServlet">アカウント設定</a></li>
+</ul>
+<div>
+<h2><a href="RoomServlet">モブルームへ</a></h2>
+</div>
+<div>
+<h2><a href="RecordServlet">記録</a></h2>
+</div>
+</body>
+</html>
--- Aproject3/src/servlet/TopServlet.java
+++ Aproject3/src/servlet/TopServlet.java
@@ -18,9 +18,25 @@ public class TopServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 		HttpServletResponse response)
 		throws ServletException, IOException {
-
-	RequestDispatcher dispatcher =
-		request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
-	dispatcher.forward(request, response);
+		
+		String jspPage = "top.jsp";
+		
+		RequestDispatcher dispatcher1 = request.getRequestDispatcher(jspPage);
+        dispatcher1.forward(request, response);
+        
+        RequestDispatcher dispatcher2 = request.getRequestDispatcher("PastdataServlet");
+        dispatcher2.forward(request, response);
+        
+        RequestDispatcher dispatcher3 = request.getRequestDispatcher("GoalServlet");
+        dispatcher3.forward(request, response);
+        
+        RequestDispatcher dispatcher4 = request.getRequestDispatcher("AccountServlet");
+        dispatcher4.forward(request, response);
+        
+        RequestDispatcher dispatcher5 = request.getRequestDispatcher("RecordServlet");
+        dispatcher5.forward(request, response);
+        
+        RequestDispatcher dispatcher6 = request.getRequestDispatcher("RoomServlet");
+        dispatcher6.forward(request, response);
 	}
 }
