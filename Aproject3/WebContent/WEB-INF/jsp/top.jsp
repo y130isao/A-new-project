9d19a5ea55d20f7ff6c0ce426bdab48b74f8d40f
snsd904513 <snsd904513@gmail.com>
2023-10-13 16:03:33 +0900

    Top,PastdataServlet変更

--- Aproject3/WebContent/WEB-INF/jsp/top.jsp
+++ Aproject3/WebContent/WEB-INF/jsp/top.jsp
@@ -9,7 +9,7 @@
 <body>
 <h1>健康管理アプリ</h1>
 <ul>
-	<li><a href="${pageContext.request.contextPath}/PastdataServlet">記録一覧</a></li>
+	<li><a href="${pageContext.request.contextPath}/PastdataServlet">過去のデータ</a></li>
 	<li><a href="${pageContext.request.contextPath}/GoalServlet">目標設定</a></li>
 	<li><a href="${pageContext.request.contextPath}/AccountServlet">アカウント設定</a></li>
 </ul>
@@ -18,6 +18,12 @@
 </div>
 <div>
 <h2><a href="${pageContext.request.contextPath}/RecordServlet">記録</a></h2>
+</div>
+
+<div>
+<p>現在のレベル: ${charaLevel}</p>
+<p>所有ポイント: ${charaPoint} point</p>
+<p>次のレベルアップまで: ${nextLevelUpPoints} point</p>
 </div>
 </body>
 </html>
--- Aproject3/src/servlet/PastdataServlet.java
+++ Aproject3/src/servlet/PastdataServlet.java
@@ -8,20 +8,38 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-
+import model.MyCalendar;
+import model.MyCalendarLogic;
 
 @WebServlet("/PastdataServlet")
+
 public class PastdataServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response)
-			throws ServletException, IOException {
-		
-		RequestDispatcher dispatcher =
-			request.getRequestDispatcher("WEB-INF/jsp/pastdata.jsp");
-		dispatcher.forward(request, response);
-		
+	  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  String s_year=request.getParameter("year");
+		  String s_month=request.getParameter("month");
+		  MyCalendarLogic logic=new MyCalendarLogic();
+			MyCalendar mc=null;
+			if(s_year != null && s_month != null) {
+				int year =Integer.parseInt(s_year);
+				int month=Integer.parseInt(s_month);
+				if(month==0) {
+					month=12;
+					year--;
+				}
+				if(month==13) {
+					month=1;
+					year++;
+				}
+				//年と月のクエリパラメーターが来ている場合にはその年月でカレンダーを生成する
+				mc=logic.createMyCalendar(year,month);
+			}else {
+				//クエリパラメータが来ていないときは実行日時のカレンダーを生成する。
+				mc=logic.createMyCalendar();
+			}
+			//リクエストスコープに格納
+			request.setAttribute("mc", mc);
+			//viewにフォワード
+			RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/jsp/pastdata.jsp");
+			rd.forward(request, response);
+		}
 	}
-}
