ddaff9c29bae42665972f8ccdcb33e8b6347eaa2
TgTakashi <mwm411000@gmail.com>
2023-10-13 11:41:09 +0900

    データベース登録機能完了＋テーブルカラム追加＋ServletとDAO修正

--- Aproject3/WebContent/WEB-INF/jsp/user.jsp
+++ Aproject3/WebContent/WEB-INF/jsp/user.jsp
@@ -10,16 +10,17 @@
 <body>
 ここはユーザーページです。<br>
 <c:choose>
-<c:when test="${account.role == 2 }">
+<c:when test="${account.roleId == 2 }">
 登録内容の確認。
 <p>ログインID：<c:out value="${account.loginId }"></c:out></p>
 <p>パスワード：<c:out value="${account.pass }"></c:out></p>
 <p>　　　名前：<c:out value="${account.name }"></c:out></p>
+<p>　　　性別：<c:out value="${account.genId }"></c:out></p>
 <p>　　ロール：ユーザー</p>
-<p><a href="/sample/Logout"><button type="button" >ログアウト</button></a></p>
+<p><a href="/Aproject3/Logout"><button type="button" >ログアウト</button></a></p>
 <a href="admin.jsp"><button type="button" >管理者ページへ</button></a>
 </c:when>
-<c:when test="${account.role == 1 }">
+<c:when test="${account.roleId == 1 }">
 <a href="admin.jsp">管理者ページへ</a>
 </c:when>
 <c:otherwise>
--- Aproject3/src/servlet/AccountCheck.java
+++ Aproject3/src/servlet/AccountCheck.java
@@ -16,45 +16,46 @@ import model.AccountBeans;
  */
 @WebServlet("/AccountCheck")
 public class AccountCheck extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AccountCheck() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AccountCheck() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
 
-        // セッションからログイン情報を取得
-        HttpSession session = request.getSession();
-        AccountBeans ab = (AccountBeans) session.getAttribute("/WEB-INF/jsp/account");
+		// セッションからログイン情報を取得
+		HttpSession session = request.getSession();
+		AccountBeans ab = (AccountBeans) session.getAttribute("account");
 
-        // ロールでフォワード先を振り分ける
-        if(ab.getRole() == 1) {
-            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/admin.jsp");
-            rd.forward(request, response);
-        } else if(ab.getRole() == 2) {
-            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/user.jsp");
-            rd.forward(request, response);
-        } else {
-            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/error.jsp");
-            rd.forward(request, response);
-        }
-    }
+		// ロールでフォワード先を振り分ける
+		if (ab.getRoleId() == 1) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/admin.jsp");
+			rd.forward(request, response);
+		} else if (ab.getRoleId() == 2) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/user.jsp");
+			rd.forward(request, response);
+		} else {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/error.jsp");
+			rd.forward(request, response);
+		}
+	}
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
-    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 }
-
