6e460885295921772b24eb56293fd19161ad7777
sakaguchi.hinatsu <sakaguchi.hinatsu@gmail.com>
2023-10-16 13:56:43 +0900

    Merge branch 'sakaguchi' of https://github.com/y130isao/A-new-project.git into sakaguchi

--- Aproject3/src/model/Content.java
+++ Aproject3/src/model/Content.java
@@ -4,12 +4,12 @@ import java.io.Serializable;
 
 public class Content implements Serializable{
 	private String exercise;
-	private String eat;
+	private String meal;
 	private String sleep;
 	public Content(){}
-	public Content(String exercise,String eat,String sleep){
+	public Content(String exercise,String meal,String sleep){
 		this.exercise=exercise;
-		this.eat=eat;
+		this.meal=meal;
 		this.sleep=sleep;
 	}
 	public String getExercise() {
@@ -18,11 +18,11 @@ public class Content implements Serializable{
 	public void setExercise(String exercise) {
 		this.exercise = exercise;
 	}
-	public String getEat() {
-		return eat;
+	public String getMeal() {
+		return meal;
 	}
-	public void setEat(String eat) {
-		this.eat = eat;
+	public void setMeal(String meal) {
+		this.meal = meal;
 	}
 	public String getSleep() {
 		return sleep;
