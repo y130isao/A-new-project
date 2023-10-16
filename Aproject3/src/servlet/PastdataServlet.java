3c7e5b26a17fde80d676c73255b9b12ad2241157
snsd904513 <snsd904513@gmail.com>
2023-10-16 16:00:32 +0900

    jarファイル配置

--- Aproject3/src/servlet/PastdataServlet.java
+++ Aproject3/src/servlet/PastdataServlet.java
@@ -12,34 +12,38 @@ import model.MyCalendar;
 import model.MyCalendarLogic;
 
 @WebServlet("/PastdataServlet")
-
 public class PastdataServlet extends HttpServlet {
-	  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		  String s_year=request.getParameter("year");
-		  String s_month=request.getParameter("month");
-		  MyCalendarLogic logic=new MyCalendarLogic();
-			MyCalendar mc=null;
-			if(s_year != null && s_month != null) {
-				int year =Integer.parseInt(s_year);
-				int month=Integer.parseInt(s_month);
-				if(month==0) {
-					month=12;
-					year--;
-				}
-				if(month==13) {
-					month=1;
-					year++;
-				}
-				//年と月のクエリパラメーターが来ている場合にはその年月でカレンダーを生成する
-				mc=logic.createMyCalendar(year,month);
-			}else {
-				//クエリパラメータが来ていないときは実行日時のカレンダーを生成する。
-				mc=logic.createMyCalendar();
-			}
-			//リクエストスコープに格納
-			request.setAttribute("mc", mc);
-			//jspにフォワード
-			RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/jsp/pastdata.jsp");
-			rd.forward(request, response);
-		}
-	}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String s_year = request.getParameter("year");
+        String s_month = request.getParameter("month");
+        MyCalendarLogic logic = new MyCalendarLogic();
+        MyCalendar mc = null;
+
+        if (s_year != null && s_month != null) {
+            int year = Integer.parseInt(s_year);
+            int month = Integer.parseInt(s_month);
+            if (month == 0) {
+                month = 12;
+                year--;
+            }
+            if (month == 13) {
+                month = 1;
+                year++;
+            }
+
+            mc = logic.createMyCalendar(year, month);
+        } else {
+            mc = logic.createMyCalendar();
+        }
+
+        request.setAttribute("mc", mc);
+
+        // Retrieve the past data
+       
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/pastdata.jsp");
+        rd.forward(request, response);
+    }
+}
