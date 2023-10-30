b1c2456cec63c772e20a8650fb05f224d8861139
sakaguchi.hinatsu <sakaguchi.hinatsu@gmail.com>
2023-10-30 11:26:51 +0900

    2023/10/30

--- Aproject3/src/dao/GoalDAO.java
+++ Aproject3/src/dao/GoalDAO.java
@@ -26,7 +26,7 @@ public class GoalDAO {
         List<Goal> goalList = new ArrayList<>();
 
         try (Connection conn = getConnection();
-             PreparedStatement pStmt = conn.prepareStatement("SELECT goalgenre1, goalgenre2, goalgenre3, goal1, goal2, goal3 FROM user_health WHERE accountId = ?")) {
+             PreparedStatement pStmt = conn.prepareStatement("SELECT goalgenre1, goalgenre2, goalgenre3, goal1, goal2, goal3 FROM account WHERE accountId = ?")) {
             pStmt.setInt(1, accountId);
 
             try (ResultSet rs = pStmt.executeQuery()) {
@@ -55,7 +55,7 @@ public class GoalDAO {
     public boolean create(Goal goal, int accountId) {
         try (Connection conn = getConnection()) {
             // アカウントIDを使用して既存の行を検索
-            String selectQuery = "SELECT COUNT(*) FROM user_health WHERE accountId = ?";
+            String selectQuery = "SELECT COUNT(*) FROM account WHERE accountId = ?";
             PreparedStatement selectStmt = conn.prepareStatement(selectQuery);
             selectStmt.setInt(1, accountId);
             ResultSet resultSet = selectStmt.executeQuery();
@@ -64,7 +64,7 @@ public class GoalDAO {
 
             if (rowCount > 0) {
                 // 既存の行がある場合は更新
-                String updateQuery = "UPDATE user_health SET goalgenre1 = ?, goalgenre2 = ?, goalgenre3 = ?, goal1 = ?, goal2 = ?, goal3 = ? WHERE accountId = ?";
+                String updateQuery = "UPDATE account SET goalgenre1 = ?, goalgenre2 = ?, goalgenre3 = ?, goal1 = ?, goal2 = ?, goal3 = ? WHERE accountId = ?";
                 PreparedStatement updateStmt = conn.prepareStatement(updateQuery);
                 updateStmt.setString(1, goal.getGoalgenre1());
                 updateStmt.setString(2, goal.getGoalgenre2());
@@ -78,7 +78,7 @@ public class GoalDAO {
                 return result == 1;
             } else {
                 // 既存の行がない場合は新規挿入
-                String insertQuery = "INSERT INTO user_health (accountId, goalgenre1, goalgenre2, goalgenre3, goal1, goal2, goal3) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                String insertQuery = "INSERT INTO account (accountId, goalgenre1, goalgenre2, goalgenre3, goal1, goal2, goal3) VALUES (?, ?, ?, ?, ?, ?, ?)";
                 PreparedStatement insertStmt = conn.prepareStatement(insertQuery);
                 insertStmt.setInt(1, accountId);
                 insertStmt.setString(2, goal.getGoalgenre1());
