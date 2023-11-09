e5d10698f7987d751213ba8e58374329e69fd6fb
sakaguchi.hinatsu <sakaguchi.hinatsu@gmail.com>
2023-11-09 16:37:26 +0900

    2023/11/09

--- Aproject3/src/model/GetGoalListLogic.java
+++ Aproject3/src/model/GetGoalListLogic.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 import dao.GoalDAO;
 
+//public class GetGoalListLogic {
+//    public List<Goal> execute(int accountId, String data_time) {
+//        GoalDAO goalDAO = new GoalDAO();
+//        List<Goal> goalList = goalDAO.getGoalsByAccountId(accountId, data_time);
+//        return goalList;
+//    
+//	}
+//}
+
+
 public class GetGoalListLogic {
     public List<Goal> execute(int accountId) {
         GoalDAO goalDAO = new GoalDAO();
